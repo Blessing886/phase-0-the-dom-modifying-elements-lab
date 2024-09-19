196adfece1cc5a1b87b50ce7926dac959e5987b8
BlessingOkora <moraablessing082@gmail.com>
2024-09-19 16:12:05 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.getElementById("main").remove();
+let newHeader = document.createElement("h1");
+newHeader.textContent = "This is the new header"; 
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.textContent = "This is the new header"; 
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Mary is the champion"; 
+document.body.appendChild(newHeader);
